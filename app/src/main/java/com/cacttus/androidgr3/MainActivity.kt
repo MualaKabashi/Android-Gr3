0a24f4ba97e7a2015f84c93911d4927ac64c7f25
Muala Kabashi <keds30955@KEDS.kv>
2024-11-21 18:47:21 +0100

    add constraint layout, view binding in activity

--- app/src/main/java/com/cacttus/androidgr3/MainActivity.kt
+++ app/src/main/java/com/cacttus/androidgr3/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.cacttus.androidgr3
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : ComponentActivity() {
+    private lateinit var editText: EditText
+    private lateinit var button5: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.relative_layout)
+        setContentView(R.layout.calculator_layout)
+
+        editText = findViewById(R.id.number)
+        button5 = findViewById(R.id.btn_5)
+
+
+        button5.setOnClickListener {
+            //use button.setText to change the value of the edit text
+            editText.setText(button5.text)
+        }
     }
 }
 
